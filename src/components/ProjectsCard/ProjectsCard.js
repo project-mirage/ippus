6580840e71627e4f73371e5e8a51d41d2b1b30cf
DaKheera47 <shaheergoodboy@gmail.com>
2021-10-21 19:04:27 +0500

    Our projects responsive

--- src/components/ProjectsCard/ProjectsCard.js
+++ src/components/ProjectsCard/ProjectsCard.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React from "react";
+import "./ProjectsCard.sass";
 
 export default function ProjectCard(props) {
-  const { opMbfmTitle, opMbfmCopy, opMbfmImage } = props;
+    const { opMbfmTitle, opMbfmCopy, opMbfmImage } = props;
 
-  return (
-    <div className="op-mbfm" data-id="148:913">
-      <h1 className="op-mbfm-title inter-bold-midnight-32px" data-id="148:914">
-        {opMbfmTitle}
-      </h1>
-      <p className="op-mbfm-copy inter-normal-blue-charcoal-16px" data-id="148:915">
-        {opMbfmCopy}
-      </p>
-      <div className="op-mbfm-image" style={{ backgroundImage: `url(${opMbfmImage})` }} data-id="148:916"></div>
-    </div>
-  );
+    return (
+        <div className="op-mbfm" data-id="148:913">
+            <div
+                className="op-mbfm-image"
+                style={{ backgroundImage: `url(${opMbfmImage})` }}
+                data-id="148:916"
+            ></div>
+            <h1
+                className="op-mbfm-title inter-bold-midnight-32px"
+                data-id="148:914"
+            >
+                {opMbfmTitle}
+            </h1>
+            <p
+                className="op-mbfm-copy inter-normal-blue-charcoal-16px"
+                data-id="148:915"
+            >
+                {opMbfmCopy}
+            </p>
+        </div>
+    );
 }
-
--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ProjectCard from "../ProjectsCard/ProjectsCard";
 import "./ProjectsSelection.sass";
+import Image from "./op-mbfm-rectangle.png";
 
 export default function App() {
     return (
         <div className="wwd-section">
-            <h1 className="hero-title-1 merriweather-bold-darkblue-136px" data-id="148:879">
+            <h1
+                className="hero-title-1 merriweather-bold-darkblue-136px"
+                data-id="148:879"
+            >
                 Our Projects
             </h1>
             <div className="project-details">
@@ -13,25 +17,25 @@ export default function App() {
                     opMbfmTitle="Melt Blown Fabric Manufacturing"
                     opMbfmCopy="Melt blowing is a conventional fabrication method of micro-and
                     nanofibers where a polymer melt is extruded through ..."
-                    opMbfmImage="op-mbfm-rectangle.png"
+                    opMbfmImage={Image}
                 />
                 <ProjectCard
                     opMbfmTitle="Melt Blown Fabric Manufacturing"
                     opMbfmCopy="Melt blowing is a conventional fabrication method of micro-and
                     nanofibers where a polymer melt is extruded through ..."
-                    opMbfmImage="op-mbfm-rectangle.png"
+                    opMbfmImage={Image}
                 />
                 <ProjectCard
                     opMbfmTitle="Melt Blown Fabric Manufacturing"
                     opMbfmCopy="Melt blowing is a conventional fabrication method of micro-and
                     nanofibers where a polymer melt is extruded through ..."
-                    opMbfmImage="op-mbfm-rectangle.png"
+                    opMbfmImage={Image}
                 />
                 <ProjectCard
                     opMbfmTitle="Melt Blown Fabric Manufacturing"
                     opMbfmCopy="Melt blowing is a conventional fabrication method of micro-and
                     nanofibers where a polymer melt is extruded through ..."
-                    opMbfmImage="op-mbfm-rectangle.png"
+                    opMbfmImage={Image}
                 />
             </div>
         </div>
