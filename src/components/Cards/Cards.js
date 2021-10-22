3a22fe4c0b29e79953765141212baa0e23c7c120
DaKheera47 <shaheergoodboy@gmail.com>
2021-10-22 23:46:53 +0500

    temporary push please send help

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -28,50 +28,48 @@ function Component5(props) {
     } = props;
 
     return (
-        <div className="component-5" data-id="189:1248">
-            <div className="overlap-group2" data-id="an|a2y9bG6X">
-                <div className="group-19 animate-enter" data-id="148:977">
-                    <div className="overlap-group" data-id="an|ySw9dhFC">
-                        <h1
-                            className="make-a-donation-titl inter-semi-bold-white-28px"
-                            data-id="148:979"
-                        >
-                            {makeADonationTitl}
-                        </h1>
-                        <p
-                            className="make-a-donation-copy-1 inter-normal-white-16px"
-                            data-id="148:980"
-                        >
-                            {makeADonationCopy}
-                        </p>
-                        <div
-                            className="make-a-donation-copy inter-medium-white-18px"
-                            data-id="148:981"
-                        >
-                            {makeADonationCopy2}
-                        </div>
+        <div className="overlap-group2" data-id="an|a2y9bG6X">
+            <div className="group-19 animate-enter" data-id="148:977">
+                <div className="overlap-group" data-id="an|ySw9dhFC">
+                    <h1
+                        className="make-a-donation-titl inter-semi-bold-white-28px"
+                        data-id="148:979"
+                    >
+                        {makeADonationTitl}
+                    </h1>
+                    <p
+                        className="make-a-donation-copy-1 inter-normal-white-16px"
+                        data-id="148:980"
+                    >
+                        {makeADonationCopy}
+                    </p>
+                    <div
+                        className="make-a-donation-copy inter-medium-white-18px"
+                        data-id="148:981"
+                    >
+                        {makeADonationCopy2}
                     </div>
                 </div>
-                <div className="group-18 animate-enter1" data-id="148:982">
-                    <div className="overlap-group1" data-id="an|PYmcYn6d">
-                        <div
-                            className="make-a-donation-titl inter-semi-bold-midnight-28px"
-                            data-id="148:984"
-                        >
-                            {makeADonationTitl2}
-                        </div>
-                        <p
-                            className="make-a-donation-copy-2 inter-normal-midnight-16px"
-                            data-id="148:985"
-                        >
-                            {makeADonationCopy3}
-                        </p>
-                        <div
-                            className="make-a-donation-copy inter-medium-midnight-18px"
-                            data-id="148:986"
-                        >
-                            {makeADonationCopy4}
-                        </div>
+            </div>
+            <div className="group-18 animate-enter1" data-id="148:982">
+                <div className="overlap-group1" data-id="an|PYmcYn6d">
+                    <div
+                        className="make-a-donation-titl inter-semi-bold-midnight-28px"
+                        data-id="148:984"
+                    >
+                        {makeADonationTitl2}
+                    </div>
+                    <p
+                        className="make-a-donation-copy-2 inter-normal-midnight-16px"
+                        data-id="148:985"
+                    >
+                        {makeADonationCopy3}
+                    </p>
+                    <div
+                        className="make-a-donation-copy inter-medium-midnight-18px"
+                        data-id="148:986"
+                    >
+                        {makeADonationCopy4}
                     </div>
                 </div>
             </div>
