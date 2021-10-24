012b79130f4ed1b31a645f9163be9452abe589e6
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2021-10-24 16:46:43 +0500

    Index page fully completed

--- src/components/HeroCTA/HeroCTA.js
+++ src/components/HeroCTA/HeroCTA.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./HeroCTA.sass";
 
 function HeroCTA(props) {
-    const { children } = props;
+    const { children, style } = props;
 
     return (
-        <div className="hero-cta animate-enter2" show-on-scroll="true">
+        <div style={style} className="hero-cta animate-enter2" show-on-scroll="true">
             <div className="label">
                 <div className="label-1 inter-semi-bold-soapstone-20px">
                     {children}
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./NavBar.sass";
+import ham from "../../images/ham.svg";
+import closeHam from "../../images/closeHam.svg";
+import useToggle from "../../hooks/useToggle";
+import HeroCTA from "../HeroCTA/HeroCTA";
 
 function NavBar() {
     return (
@@ -17,36 +21,63 @@ function NavBar() {
 export default NavBar;
 
 function NavDesktop(props) {
+    const [isNavOpen, toggleisNavOpen] = useToggle();
     const { navLeft, navHomeLink, navHomeLink2, navHomeLink3, navCta } = props;
 
     return (
         <div className="nav-desktop animate-enter1" data-id="148:989">
-            {/* <div className="nav-elements" data-id="I148:989;113:582"> */}
-                <img
-                    className="nav-left"
-                    data-id="I148:989;113:583"
-                    src={navLeft}
-                />
-                <div
-                    className="nav-right inter-medium-blue-charcoal-18px"
-                    data-id="I148:989;113:586"
-                >
-                    <div className="nav-home-link" data-id="I148:989;113:587">
-                        {navHomeLink}
-                    </div>
-                    <div className="nav-home-link-1" data-id="I148:989;113:588">
-                        {navHomeLink2}
-                    </div>
-                    <div className="nav-home-link-2" data-id="I148:989;113:589">
-                        {navHomeLink3}
-                    </div>
-                    <img
-                        className="nav-cta animate-enter"
-                        data-id="I148:989;113:590"
-                        src={navCta}
-                    />
+            <img
+                className="nav-left"
+                data-id="I148:989;113:583"
+                src={navLeft}
+            />
+            <div
+                className="nav-right inter-medium-blue-charcoal-18px"
+                data-id="I148:989;113:586"
+            >
+                <div className="nav-home-link" data-id="I148:989;113:587">
+                    {navHomeLink}
                 </div>
-            {/* </div> */}
+                <div className="nav-home-link-1" data-id="I148:989;113:588">
+                    {navHomeLink2}
+                </div>
+                <div className="nav-home-link-2" data-id="I148:989;113:589">
+                    {navHomeLink3}
+                </div>
+                <img
+                    className="nav-cta animate-enter"
+                    data-id="I148:989;113:590"
+                    src={navCta}
+                />
+            </div>
+
+            <div className="nav-right-mobile">
+                {!isNavOpen ? (
+                    <img
+                        onClick={toggleisNavOpen}
+                        src={ham}
+                        alt="hamburger icon to open menu"
+                    />
+                ) : (
+                    <img
+                        onClick={toggleisNavOpen}
+                        src={closeHam}
+                        alt="cross icon to close menu"
+                    />
+                )}
+                {isNavOpen && (
+                    <div className="nav-mobile">
+                        <p>Home</p>
+                        <p>About</p>
+                        <p>Contact</p>
+                        <HeroCTA
+                            style={{ marginTop: "20px", marginBottom: "20px" }}
+                        >
+                            Donate Now
+                        </HeroCTA>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
