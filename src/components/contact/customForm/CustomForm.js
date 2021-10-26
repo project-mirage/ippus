b69f5d35a729a0686756f45b5617a393126ad5f7
DaKheera47 <shaheergoodboy@gmail.com>
2021-10-26 16:04:53 +0500

    Custom Form Completed

--- src/components/contact/customForm/CustomForm.js
+++ src/components/contact/customForm/CustomForm.js
@@ -2,12 +2,23 @@ import React from "react";
 import * as css from "./CustomForm.module.sass";
 import FormInput from "./FormInput";
 
-export default function CustomForm({ heading, boldSubtitle }) {
+export default function CustomForm({
+    heading,
+    boldSubtitle,
+    description,
+    address,
+    email,
+    contactNumber,
+}) {
     return (
         <div className={css.formContainer}>
             <h1 className={css.heading}>{heading}</h1>
             <div className={css.left}>
                 <h2 className={css.boldSubtitle}>{boldSubtitle}</h2>
+                <a className={css.description}>{description}</a>
+                <a className={css.address}>{address}</a>
+                <a className={css.email}>{email}</a>
+                <a className={css.contactNumber}>{contactNumber}</a>
             </div>
             <div className={css.right}>
                 <form className={css.form} action="">
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,14 @@ export default function ContactPage() {
     return (
         <>
             <NavBar />
-            <CustomForm heading="Contact" />
+            <CustomForm
+                boldSubtitle="We are waiting for your email!"
+                description="Our team will get back to you ASAP with you query. In the mean time you can go through our website to get to know our values, mission and goals. "
+                address="2952 Lucia Ct, McKinney, TX 75072, USA"
+                email="nomaanaleemmufti@hotmail.com"
+                contactNumber="+1 469 931 8345"
+                heading="Contact"
+            />
             <Footer />
         </>
     );
